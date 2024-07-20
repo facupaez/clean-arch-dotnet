0fd2da2e32abd39a43dcc15d06d2976154170b8b
facupaez <facu.pz10@gmail.com>
2024-07-20 03:23:58 -0300

    Se agrega proyecto AppVenta.Infraestructura.API

---
+++ AppVenta.Infraestructura.API/Controllers/ProductoController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+using AppVenta.Dominio;
+using AppVenta.Aplicaciones.Servicios;
+using AppVenta.Infraestructura.Datos.Contextos;
+using AppVenta.Infraestructura.Datos.Repositorios;
+
+namespace AppVenta.Infraestructura.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductoController : ControllerBase
+    {
+
+        ProductoServicio CrearServicio()
+        {
+            VentaContexto db = new VentaContexto();
+            ProductoRepositorio repo = new ProductoRepositorio(db);
+            ProductoServicio servicio = new ProductoServicio(repo);
+
+            return servicio;
+        }
+
+        // GET: api/<ProductoController>
+        [HttpGet]
+        public ActionResult<List<Producto>> Get()
+        {
+            var servicio = CrearServicio();
+            return Ok(servicio.Listar());
+        }
+
+        // GET api/<ProductoController>/5
+        [HttpGet("{id}")]
+        public ActionResult<Producto> Get(Guid id)
+        {
+            var servicio = CrearServicio();
+            return Ok(servicio.SeleccionarPorId);
+        }
+
+        // POST api/<ProductoController>
+        [HttpPost]
+        public ActionResult Post([FromBody] Producto producto)
+        {
+            var servicio = CrearServicio();
+            servicio.Agregar(producto);
+            return Ok("Producto agregado.");
+        }
+
+        // PUT api/<ProductoController>/5
+        [HttpPut("{id}")]
+        public ActionResult Put(Guid id, [FromBody] Producto producto)
+        {
+            var servicio = CrearServicio();
+            producto.productoId = id;
+            servicio.Editar(producto);
+            return Ok("Producto modificado.");
+        }
+
+        // DELETE api/<ProductoController>/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(Guid id)
+        {
+            var servicio = CrearServicio();
+            servicio.Eliminar(id);
+            return Ok("Producto eliminado.");
+        }
+    }
+}
---
+++ AppVenta.Infraestructura.API/Controllers/VentaController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+using AppVenta.Dominio;
+using AppVenta.Aplicaciones.Servicios;
+using AppVenta.Infraestructura.Datos.Contextos;
+using AppVenta.Infraestructura.Datos.Repositorios;
+
+namespace AppVenta.Infraestructura.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VentaController : ControllerBase
+    {
+
+        VentaServicio CrearServicio()
+        {
+            VentaContexto db = new VentaContexto();
+            VentaRepositorio repoVenta = new VentaRepositorio(db);
+            ProductoRepositorio repoProducto = new ProductoRepositorio(db);
+            VentaDetalleRepositorio repoVentaDetalle = new VentaDetalleRepositorio(db);
+            VentaServicio servicio = new VentaServicio(repoVenta, repoProducto, repoVentaDetalle);
+
+            return servicio;
+        }
+
+        // GET: api/<VentaController>
+        [HttpGet]
+        public ActionResult<List<Venta>> Get()
+        {
+            var servicio = CrearServicio();
+            return Ok(servicio.Listar());
+        }
+
+        // GET api/<VentaController>/5
+        [HttpGet("{id}")]
+        public ActionResult<Venta> Get(Guid id)
+        {
+            var servicio = CrearServicio();
+            return Ok(servicio.SeleccionarPorId(id));
+        }
+
+        // POST api/<VentaController>
+        [HttpPost]
+        public ActionResult Post([FromBody] Venta venta)
+        {
+            var servicio = CrearServicio();
+            servicio.Agregar(venta);
+            return Ok("Venta guardada.");
+        }
+
+        // DELETE api/<VentaController>/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(Guid id)
+        {
+            var servicio = CrearServicio();
+            servicio.Anular(id);
+            return Ok("Venta anulada.");
+        }
+    }
+}
